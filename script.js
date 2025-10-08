89e3d63ce50f72b7ec0e7f90c88ffee75141577c
Joseph Orekoya <j.o.orekoya@wustl.edu>
2025-10-08 15:53:00 -0500

    Added a Javascript function that changes the color of the background of the page from light blue and light pink with the click of a button

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-alert("Hello, World!")
+// alert("Hello, World!")
 
- let changeColorButton = document.querySelector("#change-color-btn");
+//  let changeColorButton = document.querySelector("#change-color-btn");
 
-    changeColorButton.addEventListener("click", function() {
-        document.body.style.backgroundColor = "lightblue";
-    });
+//     changeColorButton.addEventListener("click", function() {
+//         document.body.style.backgroundColor = "lightblue";
+//     });
+
+
+function toggleColor() {
+            if (document.body.style.backgroundColor === 'lightblue') {
+                document.body.style.backgroundColor = 'lightpink';
+            } else {
+                document.body.style.backgroundColor = 'lightblue';
+            }
+        }
