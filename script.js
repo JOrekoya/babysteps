5bdba3a416acf1d9796a69205595c0fe73ba065d
Joseph Orekoya <j.o.orekoya@wustl.edu>
2025-10-08 21:16:23 -0500

    Change the button to change colors between light blue, light pink, and back to white. Started attempting to add a way to log feeding and sleeping events with Javascript. Cleaned up code a little bit as well.

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 // alert("Hello, World!")
 
-//  let changeColorButton = document.querySelector("#change-color-btn");
+//   let changeColorButton = document.querySelector("#change-color-btn");
 
 //     changeColorButton.addEventListener("click", function() {
 //         document.body.style.backgroundColor = "lightblue";
 //     });
 
+let toggleColorButton = document.querySelector("#toggle-color");
+toggleColorButton.addEventListener("click", toggleColor);
+
+let sleepStartValue = document.querySelector("#sleep-start").value;
+let sleepEndValue = document.querySelector("#sleep-end").value;
+
 
 function toggleColor() {
-            if (document.body.style.backgroundColor === 'lightblue') {
+            if (document.body.style.backgroundColor == 'lightblue') {
                 document.body.style.backgroundColor = 'lightpink';
-            } else {
+            } else if (document.body.style.backgroundColor == 'lightpink') {
+                document.body.style.backgroundColor = '';
+            } else if (document.body.style.backgroundColor == '') {
                 document.body.style.backgroundColor = 'lightblue';
             }
         }
+
+function logFeeding() {
+    
+
+}
